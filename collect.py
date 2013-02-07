2fe8d240970106d7d7eeb49b752af1cb1bbc9ea5
Maciej Konieczny <hello@narf.pl>
2013-02-07 23:04:11 +0100

    added script

---
+++ collect.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from __future__ import absolute_import, division
+from os import listdir, system
+from os.path import dirname, expanduser, join, realpath
+
+
+CURRENT_DIR = dirname(realpath(__file__))
+SCREENSHOTS_DIR = join(CURRENT_DIR, 'screenshots')
+
+
+def get_iso_date(path):
+    # "foo/Photo 13.11.2012 14 28 18.png" -> ("2012", "11", "13")
+    return '-'.join(path.split(' ')[-4].split('.')[::-1])
+
+
+def get_paths(dir_='~/Dropbox/homescreen'):
+    dir_ = realpath(expanduser(dir_))
+    for name in listdir(dir_):
+        yield join(dir_, name)
+
+
+def sort_paths(paths):
+    return sorted(paths, key=get_iso_date)
+
+
+def commit(path):
+    iso_date = get_iso_date(path)
+    system("""
+    mv '%(src)s' '%(dst)s'
+    convert '%(dst)s' -resize 75%% '%(curr)s'
+    optipng '%(dst)s'
+    optipng '%(curr)s'
+    git add .
+    git commit --message %(date)s
+    """.strip().replace('\n', ' && ') % {
+        'src': path,
+        'dst': join(SCREENSHOTS_DIR, iso_date + '.png'),
+        'curr': join(CURRENT_DIR, 'current_480.png'),
+        'date': iso_date,
+    })
+
+
+def _main():
+    for path in sort_paths(get_paths()):
+        commit(path)
+
+if __name__ == '__main__':
+    _main()
