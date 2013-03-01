7961a3f23cb3d63432f65f23354d87926c355059
Maciej Konieczny <hello@narf.pl>
2013-03-01 21:26:35 +0100

    bought an iPad

--- README.markdown
+++ README.markdown
@@ -1,12 +1,14 @@
 homescreen
 ==========
 
-Screen shots of my iPhone's home screen, taken once a week.
+Screen shots of my iOS home screens, taken más o menos once a week.
 
 Hat tip to Reminders.app and GitHub's awesome image diffs.
 
 
-latest screen shot
-------------------
+latest screen shots
+-------------------
 
-[![home screen](latest_480.png)](https://github.com/narfdotpl/homescreen/commits/master/latest_480.png)
+[![iPhone](latest/iPhone_480.png)](https://github.com/narfdotpl/homescreen/commits/master/latest/iPhone_480.png)
+
+[![iPad](latest/iPad_540.png)](https://github.com/narfdotpl/homescreen/commits/master/latest/iPad_540.png)
--- collect.py
+++ collect.py
@@ -7,7 +7,9 @@ from os.path import dirname, expanduser, join, realpath
 
 
 CURRENT_DIR = dirname(realpath(__file__))
-SCREENSHOTS_DIR = join(CURRENT_DIR, 'screenshots')
+ARCHIVE_DIR = join(CURRENT_DIR, 'archive')
+LATEST_DIR = join(CURRENT_DIR, 'latest')
+HOMESCREEN_DROPBOX_DIR = expanduser('~/Dropbox/homescreen')
 
 
 def get_iso_date(path):
@@ -15,8 +17,7 @@ def get_iso_date(path):
     return '-'.join(path.split(' ')[-4].split('.')[::-1])
 
 
-def get_paths(dir_='~/Dropbox/homescreen'):
-    dir_ = realpath(expanduser(dir_))
+def get_paths(dir_):
     for name in listdir(dir_):
         if name.endswith('.png'):
             yield join(dir_, name)
@@ -26,26 +27,37 @@ def sort_paths(paths):
     return sorted(paths, key=get_iso_date)
 
 
-def commit(path):
-    iso_date = get_iso_date(path)
+def commit(src_path, dst_path, latest_path, latest_scale, commit_msg):
     system("""
     mv '%(src)s' '%(dst)s'
-    convert '%(dst)s' -resize 75%% '%(latest)s'
+    convert '%(dst)s' -resize %(scale)s '%(latest)s'
     optipng '%(dst)s'
     optipng '%(latest)s'
     git add .
-    git commit --message %(date)s
+    git commit --message '%(msg)s'
     """.strip().replace('\n', ' && ') % {
-        'src': path,
-        'dst': join(SCREENSHOTS_DIR, iso_date + '.png'),
-        'latest': join(CURRENT_DIR, 'latest_480.png'),
-        'date': iso_date,
+        'src': src_path,
+        'dst': dst_path,
+        'scale': latest_scale,
+        'latest': latest_path,
+        'msg': commit_msg,
     })
 
 
 def _main():
-    for path in sort_paths(get_paths()):
-        commit(path)
+    for device, latest_suffix, latest_scale in [
+        ('iPhone', '480', '75%'),
+        ('iPad', '540', '35%'),
+    ]:
+        src_dir = join(HOMESCREEN_DROPBOX_DIR, device)
+        dst_dir = join(ARCHIVE_DIR, device)
+        latest_path = join(LATEST_DIR, '%s_%s.png' % (device, latest_suffix))
+
+        for src_path in sort_paths(get_paths(src_dir)):
+            iso_date = get_iso_date(src_path)
+            dst_path = join(dst_dir, iso_date + '.png')
+            commit_msg = '%s: %s' % (iso_date, device)
+            commit(src_path, dst_path, latest_path, latest_scale, commit_msg)
 
 if __name__ == '__main__':
     _main()
